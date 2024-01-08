edc6571f772ecae726d3ce58fa7ba3cab38ad7cc
Leonardo Pereira <leonardo_human@hotmail.com>
2024-01-08 08:50:20 -0300

    created: pages folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme.js'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
