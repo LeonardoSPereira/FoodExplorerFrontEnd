c7c74b95eafd55b5f13cf08d58ea66bdbfb31146
Leonardo Pereira <leonardo_human@hotmail.com>
2024-01-10 11:08:40 -0300

    created: not found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,13 @@
+import { Logo } from '../../components/Logo'
+import { Container } from './styles'
 import { Link } from 'react-router-dom'
 
 export function NotFound() {
   return (
-    <div>
+    <Container>
+      <Logo />
       <h1>404 - Página não encontrada</h1>
       <Link to="/">Voltar para a página inicial</Link>
-    </div>
+    </Container>
   )
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -32,7 +32,7 @@ export default createGlobalStyle`
   }
 
   button:hover, a:hover {
-    filter: brightness(0.9);
+    filter: brightness(0.8);
   }
 
 `
