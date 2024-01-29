cd715809f27af93563173c5c98d9f13ee253a960
Leonardo Pereira <leonardo_human@hotmail.com>
2024-01-29 16:08:27 -0300

    Update import path for Routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme.js'
 import GlobalStyles from './styles/global'
-import { Routes } from './routes/index'
+import { Routes } from './routes'
 import { AuthProvider } from './hooks/auth'
 import { CartProvider } from './hooks/cart'
 
